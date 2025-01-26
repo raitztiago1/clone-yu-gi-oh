175b800e2da4d84f6b93fdc1b91acc617b14f25c
tiago <raitztiago1@gmail.com>
2025-01-26 20:50:50 -0300

    Correção dos bbotões, add favicon

--- src/javascript/index.js
+++
@@ -1,9 +0,0 @@
-const btnAvancar = document.getElementById("btn-avancar");
-const btnVoltar = document.getElementById("btn-voltar");
-const cartoes = document.querySelectorAll(".cartao");
-btnAvancar.addEventListener("click", function () {
-    console.log(cartoes[0].classList)
-});
-
-btnVoltar.addEventListener("click", function () {
-});
