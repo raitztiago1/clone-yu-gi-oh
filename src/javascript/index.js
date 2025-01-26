b9584925877e06c8eb325af2f2b24d5ffd5d117b
Tiago Raitz <raitztiago1@gmail.com>
2025-01-26 12:43:59 -0300

    iniciando js

---
+++ src/javascript/index.js
@@ -0,0 +1,9 @@
+const btnAvancar = document.getElementById("btn-avancar");
+const btnVoltar = document.getElementById("btn-voltar");
+const cartoes = document.querySelectorAll(".cartao");
+btnAvancar.addEventListener("click", function () {
+    console.log(cartoes[0].classList)
+});
+
+btnVoltar.addEventListener("click", function () {
+});
